9085f92f369a4088ffdde264fa1b6bf59c021bda
biswajitsahu1 <biswajitsahu697@gmail.com>
2023-05-26 19:06:00 +0530

    boilerplatey

---
+++ Medicines/src/main/java/com/medishop/Medicines/Medicines/MedicinesApplication.java
@@ -0,0 +1,13 @@
+package com.medishop.Medicines.Medicines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicinesApplication.class, args);
+	}
+
+}
